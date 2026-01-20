5692ad6cde781e5ed313838383ac41513bcdae89
Arun kumar <arunkumar@Aruns-MacBook-Air.local>
2026-01-20 20:24:53 +0530

    gridView-buggy

--- digia-ui/src/main/java/com/digia/digiaui/framework/widgets/Builders.kt
+++ digia-ui/src/main/java/com/digia/digiaui/framework/widgets/Builders.kt
@@ -21,8 +21,8 @@ fun DefaultVirtualWidgetRegistry.registerBuiltInWidgets() {
     // Register layout widgets
     register("digia/column", ::columnBuilder)
     register("digia/row", ::rowBuilder)
-//    register("digia/stack", ::stackBuilder)
-    
+    //    register("digia/stack", ::stackBuilder)
+
     // Register list widget
     register("digia/listView", ::listViewBuilder)
 
@@ -32,9 +32,9 @@ fun DefaultVirtualWidgetRegistry.registerBuiltInWidgets() {
     register("digia/conditionalItem", ::conditionalItemBuilder)
 
     // Register Scaffold widget (commented out for now)
-     register("fw/scaffold", ::scaffoldBuilder)
+    register("fw/scaffold", ::scaffoldBuilder)
     // Register AppBar widget
-     register("digia/appBar", ::appBarBuilder)
+    register("digia/appBar", ::appBarBuilder)
     register("fw/appBar", ::appBarBuilder)
     register("digia/circularProgressBar", ::circularProgressBarBuilder)
     register("digia/futureBuilder", ::futureBuilder)
@@ -49,50 +49,55 @@ fun DefaultVirtualWidgetRegistry.registerBuiltInWidgets() {
     register("digia/wrap", ::wrapBuilder)
 
     register("digia/stack", ::stackBuilder)
-   register("digia/styledHorizontalDivider", ::styledHorizontalDividerBuilder)
+    register("digia/styledHorizontalDivider", ::styledHorizontalDividerBuilder)
 
-    register("fw/sizedBox",::sizedBoxBuilder)
-    register("fw/sized_box",::sizedBoxBuilder)
-    register("digia/gridView",::dummyBuilder)
-    register("digia/richText",::dummyBuilder)
-    register("digia/calendar",::dummyBuilder)
+    register("fw/sizedBox", ::sizedBoxBuilder)
+    register("fw/sized_box", ::sizedBoxBuilder)
+    register("digia/gridView", ::gridViewBuilder)
+    register("gridView", ::gridViewBuilder) // Alias
+    register("digia/masonryGridView", ::gridViewBuilder)
+    register("digia/calendar", ::dummyBuilder)
 }
 
-
+// Update dummyBuilder signature and call
 fun dummyBuilder(
-    nodeData: VWNodeData,
-    parent: VirtualNode?,
-    registry: VirtualWidgetRegistry
+        nodeData: VWNodeData,
+        parent: VirtualNode?,
+        registry: VirtualWidgetRegistry
 ): VirtualNode {
-  return  VWDummy(
-        refName = nodeData.refName,
-        commonProps = nodeData.commonProps,
-        parent = parent,
-        parentProps = nodeData.parentProps,
-        props = nodeData.props
+    return VWDummy(
+            type = nodeData.type, // Pass type
+            refName = nodeData.refName,
+            commonProps = nodeData.commonProps,
+            parent = parent,
+            parentProps = nodeData.parentProps,
+            props = nodeData.props
     )
 }
 
-
 class VWDummy(
-    refName: String?,
-    commonProps: CommonProps?,
-    parent: VirtualNode?,
-    parentProps: Props? = null,
-    props: Props
-) : VirtualLeafNode<Props>(
-    props = props,
-    commonProps = commonProps,
-    parent = parent,
-    refName = refName,
-    parentProps = parentProps
-) {
+        val type: String, // Add type property
+        refName: String?,
+        commonProps: CommonProps?,
+        parent: VirtualNode?,
+        parentProps: Props? = null,
+        props: Props
+) :
+        VirtualLeafNode<Props>(
+                props = props,
+                commonProps = commonProps,
+                parent = parent,
+                refName = refName,
+                parentProps = parentProps
+        ) {
 
     @Composable
     override fun Render(payload: RenderPayload) {
+        val message = payload.evalExpr(ExprOr.fromValue(props.get("message"))) ?: "No Message"
+
         Text(
-            text = "Dummy Widget Rendered",
-            modifier = Modifier.buildModifier(payload)
+                text = "DUMMY [${type}]\nMsg: $message\nKeys: ${props.value.keys}",
+                modifier = Modifier.buildModifier(payload)
         )
     }
 }
