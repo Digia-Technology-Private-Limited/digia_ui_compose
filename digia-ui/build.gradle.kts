42eb34f00a55cfdb5ed286d86610e8286a8695de
RamSuthar-Digia <ramsuthar@digia.tech>
2026-02-05 19:23:48 +0530

    version bump to 1.0.0-beta.5

--- digia-ui/build.gradle.kts
+++ digia-ui/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.digia"
-version = "1.0.0-beta.4"
+version = "1.0.0-beta.5"
 
 android {
     namespace = "com.digia.digiaui"
@@ -161,7 +161,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.digia"
                 artifactId = "digia-ui"
-                version = "1.0.0-beta.4"
+                version = "1.0.0-beta.5"
 
                 pom {
                     name.set("Digia UI Compose")
